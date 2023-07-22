0c1b88dcc9872a507aaea7aedc7b2ff0a34745e6
Bhavna Thacker <bhavnathacker14@gmail.com>
2023-07-22 13:39:20 +0530

    Demo code for Ganpat University

--- app/src/main/java/com/bhavnathacker/jetuserprofile/MainActivity.kt
+++ app/src/main/java/com/bhavnathacker/jetuserprofile/MainActivity.kt
@@ -3,19 +3,32 @@ package com.bhavnathacker.jetuserprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -28,7 +41,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetUserProfileTheme {
-                UserProfile()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UserProfile()
+                }
             }
         }
     }
@@ -36,65 +55,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UserProfile() {
+    //Text("Hello World!")
     var isClicked by remember { mutableStateOf(false) }
 
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        Card(
-            modifier = Modifier.padding(8.dp),
-            elevation = 4.dp,
-            shape = MaterialTheme.shapes.medium
-        ) {
-            Column(
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                UserImage(modifier = Modifier.size(160.dp))
-                Divider(thickness = 2.dp)
-                Text(
-                    "Bhavna Thacker",
-                    style = MaterialTheme.typography.h5,
-                    color = MaterialTheme.colors.primary
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Android Developer",
-                    style = MaterialTheme.typography.body1,
-                    color = MaterialTheme.colors.primaryVariant
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(
-                    onClick = { isClicked = !isClicked },
-                ) {
-                    Text(text = "Profile")
-                }
+    Column(verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        UserImage(modifier = Modifier.size(160.dp))
+        Divider(thickness = 2.dp)
+        Text(text = "Bhavna Thacker", style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Android Developer", style = MaterialTheme.typography.bodyMedium,
+        color = MaterialTheme.colorScheme.secondary)
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { isClicked = !isClicked }) {
+           Text(text = "Profile")
+        }
 
-                if (isClicked) {
-                    Profile(getProfileEntries())
-                }
-            }
+        if(isClicked) {
+            Profile(getProfileEntries())
         }
     }
 }
 
 @Composable
 fun Profile(entries: List<ProfileEntry>) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(8.dp)
-    ) {
-        LazyColumn {
-            items(entries) { entry ->
-                ProfileCard(entry)
-            }
+    LazyColumn {
+        items(entries) { entry ->
+            ProfileCard(entry)
         }
     }
-
 }
 
 @Composable
@@ -102,7 +92,7 @@ fun ProfileCard(entry: ProfileEntry) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp), shape = RectangleShape, elevation = 4.dp
+            .padding(8.dp), shape = RectangleShape
     ) {
         Row(
             modifier = Modifier.padding(start = 12.dp),
@@ -122,20 +112,17 @@ private fun UserImage(modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier
             .padding(8.dp),
-        shape = CircleShape,
-        elevation = 2.dp
+        shape = CircleShape
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_user_profile),
-            contentDescription = stringResource(id = R.string.user_image)
+            contentDescription = stringResource(id = R.string.app_name)
         )
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    JetUserProfileTheme {
-        UserProfile()
-    }
+fun UserProfilePreview() {
+    UserProfile()
 }
